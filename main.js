be107e15acb1beba7dba2ac3cb08d0578abf3b7d
wangxinwei18 <wangxinwei18@hotmail.com>
2021-02-19 22:49:57 +0800

    3nd commit

--- main.js
+++ main.js
@@ -26,14 +26,19 @@ mongoose.Promise = global.Promise;
 //     }
 // );
 
+// mongoose.connect(
+//     process.env.MONGODB_URI || "mongodb+srv://recipe_app:abcdef1234@cluster0.21gh5.mongodb.net/recipe_app",
+//     { useNewUrlParser: true, useFindAndModify: false,
+//       useUnifiedTopology: true
+//     }
+// );
+
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb+srv://recipe_app:abcdef1234@cluster0.21gh5.mongodb.net/recipe_app",
-    { useNewUrlParser: true, useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-);
-
-
+        process.env.MONGODB_URI || "mongodb://localhost:27017/recipe_db",
+            { useNewUrlParser: true, useFindAndModify: false,
+              useUnifiedTopology: true
+            }
+        );
 
 mongoose.set("useCreateIndex", true);
 
